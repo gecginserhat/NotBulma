f4c0913acb51868e8a6cf87f30f0a38ce97e7760
Serhat GECGIN <74310970+gecginserhat@users.noreply.github.com>
2021-04-10 21:13:32 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+$notu = 75;
+if( $notu >=0 && $notu <=29){
+    echo "Notunuz: FF";
+}
+else if($notu >=30 && $notu<=59){
+    echo "Notunuz: DD";
+}
+
+else if($notu >=60 && $notu<=67){
+    echo "Notunuz: CC";
+}
+
+else if($notu >=68 && $notu<=75){
+    echo "Notunuz: CB";
+}
+
+else if($notu >=76 && $notu<=83){
+    echo "Notunuz: BB";
+}
+
+else if($notu >=84 && $notu<=91){
+    echo "Notunuz: BA";
+}
+
+else if($notu >=92 && $notu<=100){
+    echo "Notunuz: AA";
+}
+
+?>
